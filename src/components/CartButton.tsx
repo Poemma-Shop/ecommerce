9ea237588b1e2a72be0d3fff6b55b824fb7176d7
Abraham <1001.28021547.ucla@gmail.com>
2026-02-26 14:04:24 -0400

    calculate total products

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -5,6 +5,8 @@ import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 
 export default function CartButton() {
   const { openCart, items } = useCart();
+  const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <button
       onClick={(e) => {
@@ -14,9 +16,9 @@ export default function CartButton() {
       className="relative p-2"
     >
       <ShoppingBagIcon className="h-6 w-6" />
-      {items.length > 0 && (
+      {totalItems > 0 && (
         <span className="absolute top-0 right-0 h-4 w-4 rounded-full bg-black text-white text-[10px] flex items-center justify-center">
-          {items.length}
+          {totalItems}
         </span>
       )}
     </button>
